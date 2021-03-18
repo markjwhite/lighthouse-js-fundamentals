bf7895bbee1a1507c039f0b4da8cb331f1a12a89
markjwhite <markwhite259@gmail.com>
2021-03-18 12:23:33 -0400

    takes and array of directional values and converts to coordinates pushed to new array

---
+++ x-marks-challenge.js
@@ -0,0 +1,24 @@
+const moves = ['north', 'north', 'west', 'west', 'north', 'east', 'north']
+
+const finalPosition = function (moves) {
+  const calculatedPosition = [];
+  let x = 0;
+  let y = 0;
+  for (const move of moves) {
+    if (move === 'north') {
+      y = y + 1;
+    } else if (move === 'south') {
+      y = y - 1;
+    }
+    if (move === 'east') {
+      x = x + 1;
+    } else if (move === 'west') {
+      x = x - 1;
+    }
+  }
+  calculatedPosition.push(x);
+  calculatedPosition.push(y);
+  return calculatedPosition;
+}
+
+console.log(finalPosition(moves));
