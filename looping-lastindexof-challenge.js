2bbc90dfd6b02087635404749f3a055ed142bb0c
markjwhite <markwhite259@gmail.com>
2021-03-18 20:14:34 -0400

    created my very own lastIndexOf function

---
+++ looping-lastindexof-challenge.js
@@ -0,0 +1,18 @@
+function lastIndexOf(array, value) {
+  if (array.length === 0) {
+    return -1;
+  }
+  for (let i = array.length - 1; i >= 0; i--) {
+    if (array[i] === value) {
+      return i;
+    } else if (i === 0) {
+      return -1;
+    }
+  }
+}
+
+console.log(lastIndexOf([0, 1, 4, 1, 2], 1), "=?", 3);
+console.log(lastIndexOf([0, 1, 4, 1, 2], 2), "=?", 4);
+console.log(lastIndexOf([0, 1, 4, 1, 2], 3), "=?", -1);
+console.log(lastIndexOf([5, 5, 5], 5), "=?", 2);
+console.log(lastIndexOf([], 3), "=?", -1);
